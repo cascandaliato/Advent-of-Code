c9782f891c6fbe8f5969e8f15565a9b973d16c43
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-01 08:31:08 +0100

    Day 1: Report Repair

---
+++ 01/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const input = fs.readFileSync('input.txt').toString().split('\r\n')
+
+const numbers = new Set(input.map(Number))
+
+function findTwo(targetSum) {
+  for (const n of numbers) {
+    if (numbers.has(targetSum - n)) {
+      return n * (targetSum - n)
+    }
+  }
+}
+
+console.log('Part One: ', findTwo(2020))
+
+function findThree(targetSum) {
+  for (const n of numbers) {
+    if (findTwo(targetSum - n)) {
+      return n * findTwo(targetSum - n)
+    }
+  }
+}
+
+console.log('Part Two: ', findThree(2020))
