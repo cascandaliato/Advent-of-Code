007838a51acc264771d929cc8b439b6a9073ed7f
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-02 09:38:18 +0100

    Use semicolon

--- 01/index.js
+++ 01/index.js
@@ -1,24 +1,24 @@
-const fs = require('fs')
-const input = fs.readFileSync('input.txt').toString().split('\r\n')
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().split('\r\n');
 
-const numbers = new Set(input.map(Number))
+const numbers = new Set(input.map(Number));
 
 function findTwo(targetSum) {
   for (const n of numbers) {
     if (numbers.has(targetSum - n)) {
-      return n * (targetSum - n)
+      return n * (targetSum - n);
     }
   }
 }
 
-console.log('Part One: ', findTwo(2020))
+console.log('Part One: ', findTwo(2020));
 
 function findThree(targetSum) {
   for (const n of numbers) {
     if (findTwo(targetSum - n)) {
-      return n * findTwo(targetSum - n)
+      return n * findTwo(targetSum - n);
     }
   }
 }
 
-console.log('Part Two: ', findThree(2020))
+console.log('Part Two: ', findThree(2020));
