d11efeef6e3afe79165c5c2c2b8cdd466d950491
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-07 09:42:13 +0100

    isolate console.log for parts one and two

--- template/index.js
+++ template/index.js
@@ -4,6 +4,14 @@ const { parse, solveOne, solveTwo } = require('./solvers');
 
 const lines = readFileLines(path.join(__dirname, 'input.txt'));
 
-console.log('Part One:', solveOne(parse(lines)));
+try {
+  console.log('Part One:', solveOne(parse(lines)));
+} catch (e) {
+  console.log(e);
+}
 
-console.log('Part Two:', solveTwo(parse(lines)));
+try {
+  console.log('Part Two:', solveTwo(parse(lines)));
+} catch (e) {
+  console.log(e);
+}
