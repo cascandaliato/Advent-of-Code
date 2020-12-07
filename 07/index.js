48ad16fc879bf10a3ada783daf081a33fa869bed
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-07 18:31:54 +0100

    Day 7: Handy Haversacks

--- 07/index.js
+++ 07/index.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const { readFileLines } = require('../utils');
-const { parse, solveOne, solveTwo } = require('./solvers');
+const { normalizeInput, solveOne, solveTwo } = require('./solvers');
 
 const lines = readFileLines(path.join(__dirname, 'input.txt'));
 
 try {
-  console.log('Part One:', solveOne(parse(lines)));
+  console.log('Part One:', solveOne(normalizeInput(lines)));
 } catch (e) {
   console.log(e);
 }
 
 try {
-  console.log('Part Two:', solveTwo(parse(lines)));
+  console.log('Part Two:', solveTwo(normalizeInput(lines)));
 } catch (e) {
   console.log(e);
 }
--- 07/solvers.js
+++ 07/solvers.js
@@ -1,66 +1,37 @@
 const BAGS_COLOR_RE = /^(\d?)\s?(.*?) bags?$/;
 
-exports.parse = lines =>
+exports.normalizeInput = lines =>
   lines
-    .map(l => l.split(' bags contain '))
+    .map(l => l.slice(0, -1).split(' bags contain '))
     .reduce(
-      (containers, [container, containees]) => ({
-        ...containers,
+      (contains, [container, containees]) => ({
+        ...contains,
         [container]:
-          containees === 'no other bags.'
+          containees === 'no other bags'
             ? []
-            : containees
-                .slice(0, -1)
-                .split(', ')
-                .map(c => ({
-                  color: c.match(BAGS_COLOR_RE)[2],
-                  number: Number(c.match(BAGS_COLOR_RE)[1]),
-                })),
+            : containees.split(', ').map(c => ({
+                color: c.match(BAGS_COLOR_RE)[2],
+                number: Number(c.match(BAGS_COLOR_RE)[1]),
+              })),
       }),
       {}
     );
 
-exports.solveOne = contains => {
-  const isContained = Object.entries(contains).reduce(
-    (acc, [container, containees]) => {
-      containees.forEach(({ color }) =>
-        color in acc
-          ? acc[color].add(container)
-          : (acc[color] = new Set([container]))
-      );
-      return acc;
-    },
-    {}
+const containsShinyGold = contains => color =>
+  +contains[color].reduce(
+    (res, { color: c }) =>
+      res || c === 'shiny gold' || containsShinyGold(contains)(c),
+    false
   );
 
-  const containsShinyGold = new Set();
-  const q = ['shiny gold'];
-  while (q.length) {
-    const color = q.pop();
-    containsShinyGold.add(color);
-    if (color in isContained) {
-      [...isContained[color]].forEach(c => q.push(c));
-    }
-  }
-  return [...containsShinyGold].filter(c => c !== 'shiny gold').length;
-};
+exports.solveOne = contains =>
+  Object.keys(contains).filter(containsShinyGold(contains)).length;
 
-exports.solveTwo = contains => {
-  const bagsInColor = {};
+const countContained = color => contains =>
+  contains[color].reduce(
+    (total, { color: c, number: n }) =>
+      total + n * (1 + countContained(c)(contains)),
+    0
+  );
 
-  const q = ['shiny gold'];
-  while (q.length) {
-    const color = q.pop();
-    if (contains[color].every(({ color: c }) => c in bagsInColor)) {
-      bagsInColor[color] = contains[color].reduce(
-        (count, { number: n, color: c }) => count + n + n * bagsInColor[c],
-        0
-      );
-    } else {
-      q.push(color);
-      contains[color].forEach(({ color: c }) => q.push(c));
-    }
-  }
-
-  return bagsInColor['shiny gold'];
-};
+exports.solveTwo = countContained('shiny gold');
