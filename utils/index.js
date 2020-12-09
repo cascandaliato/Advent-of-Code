4c39bedf861256583dd26e5b20b6051703004d02
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-09 08:48:20 +0100

    add Counter utility

--- utils/index.js
+++ utils/index.js
@@ -18,3 +18,26 @@ exports.tests = (suite, fn, tests) => {
     });
   });
 };
+
+exports.Counter = class Counter extends Map {
+  constructor(iter = []) {
+    super();
+    for (const key of iter) {
+      this.increment(key);
+    }
+  }
+
+  increment(key) {
+    if (this.has(key)) {
+      this.set(key, this.get(key) + 1);
+    } else {
+      this.set(key, 1);
+    }
+  }
+
+  decrement(key) {
+    if (!this.has(key)) throw new Error(`Invalid key ${key}`);
+    this.set(key, this.get(key) - 1);
+    if (this.get(key) === 0) this.delete(key);
+  }
+};
