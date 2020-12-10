937872e7a192a3b7f892fedea216b4a20af22e79
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-10 18:20:41 +0100

    Day 10: Adapter Array

--- 10/solvers.js
+++ 10/solvers.js
@@ -2,35 +2,27 @@ const { countBy } = require('lodash');
 
 exports.normalizeInput = lines => {
   const numbers = lines.map(Number).sort((a, b) => a - b);
-  return [0, ...numbers, numbers[numbers.length - 1] + 3];
+  return [...numbers, numbers.slice(-1)[0] + 3];
 };
 
 exports.solveOne = outputs => {
   const { 1: ones, 3: threes } = countBy(
-    outputs.slice(1).map((output, idx) => output - outputs[idx])
+    outputs.map((output, idx) => output - (outputs[idx - 1] || 0))
   );
   return ones * threes;
 };
 
 exports.solveTwo = outputs => {
-  const memo = {};
+  const combos = new Map([[0, 1]]);
 
-  const dp = start => {
-    if (start in memo) return memo[start];
-    if (start === outputs.length - 1) return 1;
+  outputs.forEach(output =>
+    combos.set(
+      output,
+      (combos.get(output - 1) || 0) +
+        (combos.get(output - 2) || 0) +
+        (combos.get(output - 3) || 0)
+    )
+  );
 
-    let combos = 0;
-    for (
-      let i = start + 1;
-      i < outputs.length && outputs[i] - outputs[start] <= 3;
-      i++
-    ) {
-      combos += dp(i);
-    }
-
-    memo[start] = combos;
-    return combos;
-  };
-
-  return dp(0);
+  return combos.get(outputs.slice(-1)[0]);
 };
