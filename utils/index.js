a94a6981cc588f4af9ba2336ea6330cd76ad9c7b
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-13 09:53:34 +0100

    Add chineseRemainder

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { flow, range } = require('lodash');
+const { flow, range, zip } = require('lodash');
 
 exports.readFileLines = path => fs.readFileSync(path).toString().split('\n');
 
@@ -44,3 +44,27 @@ exports.rotateCW = rotate(1);
 exports.rotateCCW = rotate(-1);
 
 exports.repeatFn = (fn, times) => flow(...range(times).fill(fn));
+
+const mulInv = (a, b) => {
+  const b0 = b;
+  let x0 = 0;
+  let x1 = 1;
+  if (b === 1) return 1;
+  while (a > 1) {
+    const q = Math.floor(a / b);
+    [a, b] = [b, a % b];
+    [x0, x1] = [x1 - q * x0, x0];
+  }
+  if (x1 < 0) x1 += b0;
+  return x1;
+};
+
+exports.chineseRemainder = (moduli, remainders) => {
+  let sum = 0;
+  const product = moduli.reduce((a, b) => a * b);
+  for (const [modulus, remainder] of zip(moduli, remainders)) {
+    const p = Math.floor(product / modulus);
+    sum += remainder * mulInv(p, modulus) * p;
+  }
+  return sum % product;
+};
