a4bff31327028494c8e9ca49a3c8e0b7b1cf7a78
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-17 19:02:48 +0100

    Day 17: Conway Cubes

--- 2020/17/solvers.js
+++ 2020/17/solvers.js
@@ -1,4 +1,4 @@
-const { combos, multiRange } = require('../../utils');
+const { combos } = require('../../utils');
 const Grid = require('../../utils/sparse-hyper-matrix');
 
 const status = { '#': 1, '.': 0 };
@@ -9,8 +9,20 @@ exports.normalizeInput = lines =>
 const directions = dimensions =>
   combos([0, -1, 1], dimensions).filter(combo => !combo.every(v => v === 0));
 
+const withNeighbors = (grid, deltas) =>
+  [
+    ...new Set([
+      ...[...grid].map(coords => coords.slice(0, -1).join(',')),
+      ...[...grid]
+        .flatMap(coords =>
+          deltas.map(delta => delta.map((d, idx) => d + coords[idx]))
+        )
+        .map(arr => arr.join(',')),
+    ]),
+  ].map(str => str.split(',').map(Number));
+
 const solve = dimensions => slice => {
-  let grid = new Grid({ dimensions, defaultValue: 0 });
+  let grid = new Grid({ defaultValue: 0 });
   slice.forEach((row, y) =>
     row.forEach((v, x) => grid.set(x, y, ...Array(dimensions - 2).fill(0), v))
   );
@@ -18,10 +30,7 @@ const solve = dimensions => slice => {
 
   for (let cycle = 1; cycle <= 6; cycle++) {
     const nextGrid = grid.clone();
-    const boundaries = nextGrid
-      .getBoundaries()
-      .map(([min, max]) => [min - 1, max + 1]);
-    multiRange(boundaries).forEach(coords => {
+    withNeighbors(nextGrid, deltas).forEach(coords => {
       const activeNeighbors = deltas
         .map(delta => grid.get(...coords.map((c, idx) => c + delta[idx])))
         .reduce((a, b) => a + b);
@@ -39,7 +48,7 @@ const solve = dimensions => slice => {
     grid = nextGrid;
   }
 
-  return [...grid].map(entry => entry.slice(-1)[0]).reduce((a, b) => a + b);
+  return [...grid.clone()].length;
 };
 
 exports.solveOne = solve(3);
