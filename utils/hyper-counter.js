719e29b8e6b1d94e588b049df84028f94ec206d0
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-18 23:17:23 +0100

    Fix counter data structure

--- utils/hyper-counter.js
+++ utils/hyper-counter.js
@@ -14,7 +14,7 @@ class HyperCounter {
   remove(coords) {
     const current = this.get(coords);
     if (current === 1) {
-      this._map.remove(coords.join(';'));
+      this._map.delete(coords.join(';'));
     } else {
       this._map.set(coords.join(';'), current - 1);
     }
@@ -22,7 +22,7 @@ class HyperCounter {
 
   *[Symbol.iterator]() {
     for (const [key, value] of this._map) {
-      yield [key.split(';'), value];
+      yield [key.split(';').map(Number), value];
     }
   }
 }
---
+++ utils/hyper-map.js
@@ -0,0 +1,30 @@
+class HyperMap {
+  constructor(defaultValue = undefined) {
+    this._defaultValue = defaultValue;
+    this._map = new Map();
+  }
+
+  get(coords) {
+    return this._map.get(coords.join(';')) || this._defaultValue;
+  }
+
+  set(coords, value) {
+    if (value === this._defaultValue) {
+      this._map.delete(coords.join(';'));
+    } else {
+      this._map.set(coords.join(';'), value);
+    }
+  }
+
+  delete(coords) {
+    this._map.delete(coords.join(';'));
+  }
+
+  *[Symbol.iterator]() {
+    for (const [key, value] of this._map) {
+      yield [key.split(';').map(Number), value];
+    }
+  }
+}
+
+module.exports = HyperMap;
