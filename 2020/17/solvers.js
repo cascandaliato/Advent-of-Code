e8afb2d270685047752c6b535d4608052ecfc02d
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-18 21:21:35 +0100

    Day 17: Conway Cubes

--- 2020/17/solvers.js
+++ 2020/17/solvers.js
@@ -1,5 +1,6 @@
 const { combos } = require('../../utils');
-const HyperCube = require('../../utils/hyper-cube');
+const HyperCounter = require('../../utils/hyper-counter');
+const HyperSet = require('../../utils/hyper-set');
 
 const status = { '#': 1, '.': 0 };
 
@@ -9,62 +10,46 @@ exports.normalizeInput = lines =>
 const directions = dimensions =>
   combos([0, -1, 1], dimensions).filter(combo => !combo.every(v => v === 0));
 
-const withNeighbors = (grid, deltas) =>
-  [
-    ...new Set([
-      ...[...grid.entries()].map(([coords]) => coords.join(',')),
-      ...[...grid.entries()]
-        .flatMap(([coords]) =>
-          deltas.map(delta => delta.map((d, idx) => d + coords[idx]))
-        )
-        .map(arr => arr.join(',')),
-    ]),
-  ]
-    .map(str => str.split(',').map(Number))
-    .filter(coords =>
-      coords.every((coord, idx) => coord >= 0 && coord < grid.lengths[idx])
-    );
-
 const solve = ({ dimensions, generations }) => slice => {
-  let cube = new HyperCube({
-    lengths: [
-      slice[0].length + generations * 2,
-      slice.length + generations * 2,
-      ...Array(dimensions - 2).fill(1 + generations * 2),
-    ],
-    defaultValue: 0,
-  });
+  let cube = new HyperSet();
+
+  const lengths = [
+    slice[0].length + generations * 2,
+    slice.length + generations * 2,
+    ...Array(dimensions - 2).fill(1 + generations * 2),
+  ];
+
   slice.forEach((row, y) =>
-    row.forEach((v, x) =>
-      cube.set(
-        x + generations,
-        y + generations,
-        ...Array(dimensions - 2).fill(generations),
-        v
-      )
+    row.forEach(
+      (v, x) =>
+        v === 1 &&
+        cube.add([
+          x + generations,
+          y + generations,
+          ...Array(dimensions - 2).fill(generations),
+        ])
     )
   );
 
   const deltas = directions(dimensions);
-  for (let cycle = 1; cycle <= generations; cycle++) {
-    const nextCube = cube.clone();
-    withNeighbors(nextCube, deltas).forEach(coords => {
-      const activeNeighbors = deltas
-        .map(delta => coords.map((c, idx) => c + delta[idx]))
-        .filter(c => c.every((v, idx) => v >= 0 && v < cube.lengths[idx]))
-        .map(c => cube.get(...c))
-        .reduce((a, b) => a + b);
 
-      if (
-        cube.get(...coords) === 1 &&
-        activeNeighbors !== 2 &&
-        activeNeighbors !== 3
-      ) {
-        nextCube.set(...coords, 0);
-      } else if (cube.get(...coords) === 0 && activeNeighbors === 3) {
-        nextCube.set(...coords, 1);
+  for (let cycle = 1; cycle <= generations; cycle++) {
+    const nextCube = new HyperSet();
+
+    const counter = new HyperCounter();
+    [...cube].forEach(coords =>
+      deltas
+        .map(delta => coords.map((c, idx) => c + delta[idx]))
+        .filter(c => c.every((v, idx) => v >= 0 && v < lengths[idx]))
+        .forEach(c => counter.add(c))
+    );
+
+    for (const [coords, value] of counter) {
+      if (value === 3 || (value === 2 && cube.has(coords))) {
+        nextCube.add(coords);
       }
-    });
+    }
+
     cube = nextCube;
   }
 
