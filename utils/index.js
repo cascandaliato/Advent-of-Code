29d9106c523a5477bfa3731c9c2ebaf5443fdec6
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-19 10:19:06 +0100

    add memoize util

--- utils/index.js
+++ utils/index.js
@@ -178,3 +178,12 @@ exports.evaluatePostfix = postfix => {
 
   return operands[0];
 };
+
+exports.memoize = fn => {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (!cache.has(key)) cache.set(key, fn(...args));
+    return cache.get(key);
+  };
+};
