213503c85ca327017992f8fb3b6d8f3df8cc40cd
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2021-12-10 08:13:53 +0000

    Day 9: Smoke Basin

--- 2021/09/__init__.py
+++ 2021/09/__init__.py
@@ -14,31 +14,39 @@ def parse(lines):
 directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
+def neighbors(loc, heights):
+    for direction in directions:
+        neighbor = tuple(x+dx for x, dx in zip(loc, direction))
+        if neighbor in heights:
+            yield neighbor
+
+
 def lows(heights):
-    return [(x, y) for (x, y), h in heights.items() if all(h < heights[(x+dx, y+dy)] for dx, dy in directions if (x+dx, y+dy) in heights)]
+    return [loc for loc, height in heights.items() if all(height < heights[neighbor] for neighbor in neighbors(loc, heights))]
+
+
+def basin_from(low, heights):
+    q, basin = [(low, -inf)], set()
+    while q:
+        loc, prev = q.pop()
+        if loc in basin or heights[loc] == 9:
+            continue
+        if heights[loc] > prev:
+            basin.add(loc)
+            q.extend([(neighbor, heights[loc])
+                     for neighbor in neighbors(loc, heights)])
+            heights[loc] = 9
+    return tuple(sorted(basin))
 
 
 @expect({'test': 15})
 def solve1(heights):
-    return sum(1+heights[(x, y)] for x, y in lows(heights))
-
-
-def basin_from(x, y, heights):
-    q, basin = [(x, y, -inf)], set()
-    while q:
-        x, y, prev = q.pop()
-        if (x, y) not in heights or (x, y) in basin or heights[(x, y)] == 9:
-            continue
-        if heights[(x, y)] > prev:
-            basin.add((x, y))
-            q.extend([(x+dx, y+dy, heights[(x, y)])
-                      for dx, dy in directions])
-    return tuple(sorted(basin))
+    return sum(1+heights[loc] for loc in lows(heights))
 
 
 @expect({'test': 1134})
 def solve2(heights):
     answer = 1
-    for basin in sorted(set(basin_from(x, y, heights) for x, y in lows(heights)), key=len, reverse=True)[:3]:
+    for basin in sorted([basin_from(low, heights) for low in lows(heights)], key=len, reverse=True)[:3]:
         answer *= len(basin)
     return answer
