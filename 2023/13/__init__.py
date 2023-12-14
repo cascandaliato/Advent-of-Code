81c0aa142471c13940b3f84c93bbe0ba9a32704e
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-12-14 01:24:00 +0000

    Year 2023 Day 13: Point of Incidence

--- 2023/13/__init__.py
+++ 2023/13/__init__.py
@@ -11,52 +11,40 @@ def parse(lines):
     return patterns
 
 
-def has_v_reflection(pattern, with_smudge=False):
-    def is_v_reflection(row, candidate, smudge_fixed):
-        diffs = 0
+def has_v_reflection(pattern, smudges=0):
+    def is_v_reflection(row, candidate, smudges_left):
         rng = min(candidate, len(row) - candidate)
         for i in range(rng):
             if row[candidate - 1 - i] != row[candidate + i]:
-                diffs += 1
-        if diffs == 0:
-            return True, smudge_fixed
-        elif with_smudge and diffs == 1 and not smudge_fixed:
-            return True, True
-        else:
-            return False, False
+                smudges_left -= 1
+        return smudges_left
 
-    candidates = [(n, False) for n in range(1, len(pattern[0]))]
+    candidates = [(n, smudges) for n in range(1, len(pattern[0]))]
     for row in pattern:
         candidates_new = []
-        for candidate, smudge_fixed in candidates:
-            is_reflection, smudge_fixed_new = is_v_reflection(
-                row, candidate, smudge_fixed
-            )
-            if is_reflection:
-                candidates_new.append((candidate, smudge_fixed_new))
+        for candidate, smudges_left in candidates:
+            smudges_left = is_v_reflection(row, candidate, smudges_left)
+            if smudges_left >= 0:
+                candidates_new.append((candidate, smudges_left))
         candidates = candidates_new
 
-    for candidate, smudge_fixed in candidates:
-        if smudge_fixed == with_smudge:
+    for candidate, smudges_left in candidates:
+        if smudges_left == 0:
             return candidate
     return 0
 
 
 def transpose(pattern):
-    transposed = [[] for _ in range(len(pattern[0]))]
-    for r in range(len(pattern)):
-        for c in range(len(pattern[0])):
-            transposed[c].append(pattern[r][c])
-    return transposed
+    return list(zip(*pattern))
 
 
-def solve(patterns, with_smudge=False):
+def solve(patterns, smudges=0):
     ans = 0
     for pattern in patterns:
-        if n := has_v_reflection(pattern, with_smudge):
+        if n := has_v_reflection(pattern, smudges):
             ans += n
         else:
-            ans += has_v_reflection(transpose(pattern), with_smudge) * 100
+            ans += has_v_reflection(transpose(pattern), smudges) * 100
     return ans
 
 
@@ -67,4 +55,4 @@ def solve1(patterns):
 
 @expect({"test": 400})
 def solve2(patterns):
-    return solve(patterns, with_smudge=True)
+    return solve(patterns, smudges=1)
